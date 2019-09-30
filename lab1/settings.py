82687f7983c203c51023bed72b982d61c4f1182d
Polinasun <black_sun-15@mail.ru>
2019-09-30 13:00:12 +0300

    mess

--- lab1/settings.py
+++ lab1/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = 'mp%fs9*9e*a&g!kcg9@e&i-w)y-mi3!_7gsva)^d2omce)p)u6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+DEBUG_COLLECTSTATIC=1
 #DEBUG = True
 ALLOWED_HOSTS = ['powerful-falls-35066.herokuapp.com']
 #
